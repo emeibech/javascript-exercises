ae5a5cea0bee24b51f837a0eeb33ab3197322bd8
emeibech <emeibech@gmail.com>
2022-07-06 15:37:49 +0800

    Completes exercise /02_repeatingString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+let newString = string;
+for (let i = 2; i <= num; i++) {
+    newString += string;
+}
+if (num == 0) {
+    return '';
+}
+else if (num < 0) {
+    return 'ERROR';
+}
+else {
+return newString;
+}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
