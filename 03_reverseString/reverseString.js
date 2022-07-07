3a8c3bb040e6a7b847bbd1a561b37b7bf0106943
emeibech <emeibech@gmail.com>
2022-07-07 09:04:00 +0800

    Creates function to reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let splitString = string.split('');
+let reversed = splitString.reverse();
+let joined = reversed.join('');
+return joined;
 };
 
 // Do not edit below this line
