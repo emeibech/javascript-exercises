531ef5da3de7526eb8047ba4d6e9d0354dd435dc
emeibech <emeibech@gmail.com>
2022-07-11 13:04:41 +0800

    Create two functions that converts celsius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+let convertedToCelsius = (fahrenheit - 32) * 5/9;
+let celsius = Number(convertedToCelsius.toFixed(1));
+if (celsius === 0.0) {
+  return 0;
+}
+else {
+return celsius;
+}
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+let convertedToFahrenheit = (celsius * 9/5) + 32;
+let fahrenheit = Number(convertedToFahrenheit.toFixed(1));
+if (fahrenheit === 0.0) {
+  return 0;
+}
+else {
+return fahrenheit;
+}
 };
 
 // Do not edit below this line
