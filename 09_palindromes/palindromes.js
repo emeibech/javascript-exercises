3b09acec6465738b9b6be7083d1854f07e0599ec
emeibech <emeibech@gmail.com>
2022-08-09 12:10:06 +0800

    Return true if passed argument is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const stringArray = [...string.toLowerCase()];
+    let processedArray = stringArray
+        .filter(punct => punct.replace(/[ !.,]/g, ''))
+        .reverse();
+    let stringed = processedArray.toString();
+      return processedArray.reverse().toString() === stringed ? true : false;
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
