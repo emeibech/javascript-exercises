50c0ccf5130725f72daf1094b9cb2ce3e6693034
emeibech <emeibech@gmail.com>
2022-08-10 07:56:25 +0800

    Return specific number of the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    if (fib < 0) return 'OOPS';
+    let prev = 0;
+    let current = 1;
+    for (; fib > 0; fib--) {
+        current += prev;
+        prev = current - prev;
+    }
+    return prev;
 };
 
 // Do not edit below this line
