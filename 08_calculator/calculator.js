a482989c0cb420b40bc5ac65563ed8aeaf602a3b
emeibech <emeibech@gmail.com>
2022-08-09 09:53:16 +0800

    Finish calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, element) =>  total += element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  const arr = args[0]
+  return arr.reduce((total, num) => total *= num);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  else {
+  let total = 1;
+	for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
+}
+}
 
 // Do not edit below this line
 module.exports = {
