0d6a102c17d5def81520c57b34be124948e37870
emeibech <emeibech@gmail.com>
2022-08-10 13:17:25 +0800

    Find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    const isOldest = people.sort((a, b) => (currentYear - a.yearOfBirth) > (currentYear - b.yearOfBirth) ? -1: 1)[0];
+    if (isOldest.yearOfDeath == undefined) return isOldest;
+    return people.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1: 1)[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
